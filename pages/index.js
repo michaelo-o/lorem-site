50be5f1d5209126a46551d235836b3b4e75c8e97
unknown <mike>
2021-09-14 13:13:39 +0100

    converted2

--- pages/index.js
+++ pages/index.js
@@ -14,16 +14,16 @@ export default function Home() {
         <Image src="/lorem-ipsum-banner.jpg" width={1000} height={350} />
       </div>
       <div className={styles.home}>
-        <h2>Welcome to The Home of Lorem Ipsum</h2>
+        <h2 className={styles.welcome}>Welcome to The Home of Lorem Ipsum</h2>
         <div className="lorem_definition">
-          <h3>What is Lorem Ipsum?</h3>
+          <h3 className={styles.headers}>What is Lorem Ipsum?</h3>
           <p><i>Lorem ipsum</i> is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
         </div>
         <div className="lorem_history">
-          <h3>HISTORY, PURPOSE AND USAGE</h3>
+          <h3 className={styles.headers}>HISTORY, PURPOSE AND USAGE</h3>
           <p><i>Lorem Ipsum</i>, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's <i>De Finibus Bonorum et Malorum</i> for use in a type specimen book. It usually begins with:</p>
 
-          <p><i>“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.”</i></p>
+          <p><i className={styles.headers}>“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.”</i></p>
 
           <p>The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.) that doesn't distract from the layout. A practice not without controversy, laying out pages with meaningless filler text can be very useful when the focus is meant to be on design, not content.</p>
 
