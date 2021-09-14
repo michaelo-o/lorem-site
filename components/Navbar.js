5a89e18e8652b2bc8db424b2740172591399395b
unknown <mike>
2021-09-14 16:30:20 +0100

    converted3

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,21 +1,36 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import Head from 'next/dist/next-server/lib/head'
+import { useContext } from 'react'
+import AuthContext from '../stores/authContext'
+import Head from 'next/head'
+
 
 export default function Navbar() {
+  const { user, login } = useContext(AuthContext)
+  console.log(user)
+
   return (
     <div className="container">
       <Head>
         <link rel="icon" href="/logo-favicon.png" />
       </Head>
-      <nav>
-        <Image src="/logo-favicon.png" width={50} height={48} />
-        <Link href="/"><a><h1>Home Of Lorem Ipsum</h1></a></Link>
-        <ul>
-          <li><Link href="/"><a>Home</a></Link></li>
-          <li><Link href="/lorems"><a>Lorem Ipsums</a></Link></li>
-        </ul>
+      <nav className="navbar">
+        <div className="logo-sitename">
+          <Image src="/logo-favicon.png" width={85} height={75} />
+          <Link href="/"><a><h1>Home Of Lorem Ipsum</h1></a></Link>
+        </div>
+          <div className="links">
+          <p><Link href="/"><a>Home</a></Link></p>
+          <p><Link href="/lorems"><a>Lorem Ipsums</a></Link></p>
+          <p onClick={login} className="btn">Login/Signup</p>
+          </div>
       </nav>
-    </div>
+
+    </div >
   )
 }
+
+
+
+
+
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,15 @@
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
+import {AuthContextProvider} from '../stores/authContext'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
+    <AuthContextProvider>
       <Navbar />
       <Component {...pageProps} />
       <Footer />
-    </div>
+    </AuthContextProvider>
   )
 }
 
