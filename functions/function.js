02669d1010118a5d959422c0e579ceb348c8402d
unknown <mike>
2021-09-16 14:45:57 +0100

    cdassvdv

--- functions/function.js
+++ functions/function.js
@@ -1,11 +1,15 @@
 exports.handler = async (event, context) => {
     console.log('function ran')
-  
-    const data = { name: 'mario', age: 35, job: 'plumber' } 
-  
+
+    const data = {
+        "id": 1,
+        "lorem_name": "in libero",
+        "lorem_body": "condimentum curabitur in libero ut massa volutpat convallis morbi odio odio elementum eu interdum eu tincidunt in leo maecenas pulvinar lobortis est phasellus sit amet erat nulla tempus vivamus in felis eu sapien cursus vestibulum proin eu mi nulla ac enim in tempor turpis nec euismod scelerisque quam"
+    }
+
     // return response to browser
     return {
-      statusCode: 200,
-      body: JSON.stringify(data)
+        statusCode: 200,
+        body: JSON.stringify(data)
     }
-  }
+}
